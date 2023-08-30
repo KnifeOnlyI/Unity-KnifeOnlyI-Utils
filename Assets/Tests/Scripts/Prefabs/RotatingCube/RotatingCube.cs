1b7f9bec4215beed745b002ce1280e072f3a1e7f
KnifeOnlyI <dany.pignoux@outlook.fr>
2023-08-30 18:11:03 +0200

    Initial commit

---
+++ Assets/Tests/Scripts/Prefabs/RotatingCube/RotatingCube.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace Prefabs.RotatingCube
+{
+    public class RotatingCube : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform cube;
+
+        private float _timer;
+        private bool _up = true;
+
+        private void Update()
+        {
+            var acc = Time.deltaTime * 0.10f;
+
+            _timer += acc;
+            
+            if (_timer > 0.25)
+            {
+                _timer = 0.0f;
+                _up = !_up;
+            }
+
+            cube.Translate(_up ? new Vector3(0, acc, 0) : new Vector3(0, -acc, 0));
+            cube.Rotate(new Vector3(0, Time.deltaTime * 10, 0));
+        }
+    }
+}
